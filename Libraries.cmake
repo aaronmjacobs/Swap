c69d688dca5ab30ddaa7ab73a689b899b73014aa
Aaron Jacobs <aaron@aaronmjacobs.com>
2018-06-17 01:32:04 -0700

    Add libraries

---
+++ Libraries.cmake
@@ -0,0 +1,40 @@
+# Boxer
+add_subdirectory("${LIB_DIR}/Boxer")
+target_link_libraries(${PROJECT_NAME} PUBLIC Boxer)
+
+# glad
+add_subdirectory("${LIB_DIR}/glad")
+target_link_libraries(${PROJECT_NAME} PUBLIC optimized glad)
+target_link_libraries(${PROJECT_NAME} PUBLIC debug glad-debug)
+
+# GLFW
+set(GLFW_BUILD_EXAMPLES OFF CACHE INTERNAL "Build the GLFW example programs")
+set(GLFW_BUILD_TESTS OFF CACHE INTERNAL "Build the GLFW test programs")
+set(GLFW_BUILD_DOCS OFF CACHE INTERNAL "Build the GLFW documentation")
+set(GLFW_INSTALL OFF CACHE INTERNAL "Generate installation target")
+add_subdirectory("${LIB_DIR}/glfw")
+target_compile_definitions(${PROJECT_NAME} PUBLIC GLFW_INCLUDE_NONE)
+target_link_libraries(${PROJECT_NAME} PUBLIC glfw)
+
+# PPK_ASSERT
+set(PPK_DIR "${LIB_DIR}/PPK_ASSERT")
+target_sources(${PROJECT_NAME} PUBLIC "${PPK_DIR}/src/ppk_assert.h" "${PPK_DIR}/src/ppk_assert.cpp")
+target_include_directories(${PROJECT_NAME} PUBLIC "${PPK_DIR}/src")
+source_group("Libraries\\PPK_ASSERT" "${PPK_DIR}/src")
+
+# templog
+set(TEMPLOG_DIR "${LIB_DIR}/templog")
+target_sources(${PROJECT_NAME} PRIVATE
+   "${TEMPLOG_DIR}/config.h"
+   "${TEMPLOG_DIR}/logging.h"
+   "${TEMPLOG_DIR}/templ_meta.h"
+   "${TEMPLOG_DIR}/tuples.h"
+   "${TEMPLOG_DIR}/type_lists.h"
+   "${TEMPLOG_DIR}/imp/logging.cpp"
+)
+target_include_directories(${PROJECT_NAME} PUBLIC "${TEMPLOG_DIR}")
+source_group("Libraries\\templog" "${TEMPLOG_DIR}")
+
+# tinyobj
+add_subdirectory("${LIB_DIR}/tinyobjloader")
+target_link_libraries(${PROJECT_NAME} PUBLIC tinyobjloader)
