71fcba8427bc5a42d945f5f193ecba4f929f21e2
Aaron Jacobs <aaron@aaronmjacobs.com>
2020-01-22 21:10:07 -0800

    Add resource files to project sources

---
+++ Resources.cmake
@@ -0,0 +1,37 @@
+set(MESH_DIR "${RES_DIR}/Meshes")
+set(SHADER_DIR "${RES_DIR}/Shaders")
+
+set(RESOURCE_FILES
+   "${MESH_DIR}/Bunny.obj"
+   "${MESH_DIR}/Cone.obj"
+   "${MESH_DIR}/Sphere.obj"
+
+   "${SHADER_DIR}/Blur.frag"
+   "${SHADER_DIR}/DeferredLighting.frag"
+   "${SHADER_DIR}/DeferredLighting.vert"
+   "${SHADER_DIR}/DepthOnly.frag"
+   "${SHADER_DIR}/DepthOnly.vert"
+   "${SHADER_DIR}/Forward.frag"
+   "${SHADER_DIR}/Forward.vert"
+   "${SHADER_DIR}/ForwardCommon.glsl"
+   "${SHADER_DIR}/FramebufferCommon.glsl"
+   "${SHADER_DIR}/GBuffer.frag"
+   "${SHADER_DIR}/GBuffer.vert"
+   "${SHADER_DIR}/GBufferCommon.glsl"
+   "${SHADER_DIR}/LightingCommon.glsl"
+   "${SHADER_DIR}/MaterialCommon.glsl"
+   "${SHADER_DIR}/MaterialDefines.glsl"
+   "${SHADER_DIR}/Normals.frag"
+   "${SHADER_DIR}/Normals.vert"
+   "${SHADER_DIR}/Screen.vert"
+   "${SHADER_DIR}/SSAO.frag"
+   "${SHADER_DIR}/SSAOBlur.frag"
+   "${SHADER_DIR}/Threshold.frag"
+   "${SHADER_DIR}/Tonemap.frag"
+   "${SHADER_DIR}/Version.glsl"
+   "${SHADER_DIR}/ViewCommon.glsl"
+)
+
+target_sources(${PROJECT_NAME} PRIVATE ${RESOURCE_FILES})
+
+source_group(TREE "${RES_DIR}" PREFIX Resources FILES ${RESOURCE_FILES})
